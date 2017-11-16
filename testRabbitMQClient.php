38f7fa497f5ebc4633eed97699cd0cdeba6b4239
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-11-16 11:40:39 -0500

    Update testRabbitMQClient.php

--- testRabbitMQClient.php
+++ testRabbitMQClient.php
@@ -3,35 +3,23 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-require_once('login.php.inc');
-function doLogin($username,$password)
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
 {
-    $login = new logindb();
-    $output = $login->validateLogin($username,$password);
-	if($output)
-		{
-			echo "success";
-		}
-	return $output;
+  $msg = $argv[1];
 }
-function requestProcessor($request)
+else
 {
-  echo "received request".PHP_EOL;
-  var_dump($request);
-  if(!isset($request['type']))
-  {
-    return "ERROR: unsupported message type";
-  }
-  switch ($request['type'])
-  {
-    case "login":
-      return doLogin($request['username'],$request['password']);
-    case "validate_session":
-      return doValidate($request['sessionId']);
-  }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+  $msg = "test message";
 }
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
-$server->process_requests('requestProcessor');
-exit();
-?>
+$request = array();
+$request['type'] = "Login";
+$request['username'] = "steve";
+$request['password'] = "password";
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+echo $argv[0]." END".PHP_EOL;
