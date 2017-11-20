b1f046bb7e1a726f73e5777c4b3ff251c2b66c44
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-11-20 15:46:20 -0500

    Create NBAhomepage.php

---
+++ NBAhomepage.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kehoe's Bros Betting</title> 
+<style>
+body {margin: 0;}
+#css1{
+  background-color: black;
+  background-position: right bottom;
+  background-repeat: repeat;
+  padding: .5px;
+}
+.topnav{
+    overflow: hidden;
+    background-color:black;
+    }
+.topnav a{
+    float: left;
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 5px 10px;
+    text-decoration: none;
+    font-size: 10px;
+    }
+.topnav a:hover{
+    background-color: lightblue;
+    color: white;
+}
+.topnav a.active{
+    background-color: #ee2211;
+    color: red;
+}   
+.box1{
+    position: relative;
+    margin: auto;
+    float: center;
+    width: 400px;
+    font-family:"georgia";
+    height: 100px;
+    background-color: pink;
+    text-align: left;
+    padding-left: .5cm;
+    padding-top: .05cm;
+    padding-bottom: .8cm;
+}
+    
+</style>
+
+</head>
+
+<body>
+<div id = "css1">
+<h3><font face="georgia" size="5" color="white">Kehoe's Bros Betting</font></h3>
+    <div class="topnav" id="firstTopNav">
+        <a href="NBAhomepage.php">HOME</a>
+        <a href="NBAdeposit.php">DEPOSIT</a>
+        <a href="about.php">ABOUT</a>
+        <a href="NBAcompareResults.php">RESULTS</a>
+    </div>
+</div>
+</body>
+</style>
+
+<!--This is for SEARCH BAR!-->
+
+</head>
+<!--<body background = "https://www.bookmakers.bet/wp-content/uploads/2016/12/Fantasy-Football-NFL-Images-2014.jpg">-->
+<body>
+<center>
+<style>
+    { margin: 0; padding: 0; }
+    html {
+        background: url('http://www.twitrcovers.com/wp-content/uploads/2014/02/NBA-Tracy-McGrady-Athlete-l.jpg') no-repeat center center fixed;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
+
+<style>
+
+.div1, .div2, .div3{
+ 
+    position: relative;
+    margin: 1%;
+    float: center;
+    width: 23%;
+    height: 250px;
+    background-color: black;
+}
+
+</style>
+<br>
+<body>
+<center>
+
+<center>
+<br>
+
+<div class="div1">
+<center>
+<br>
+<form action="NBAbet.php" method="get">
+  <font face="impact" color="white" size="5"><b>Select 3 Teams</b></font>
+  <br><br>
+  <select id = "basketball" name="basketball" style="width: 200px; border: 0px solid; padding: 5px; color: #000; background-color: #f1f1f1;  font-family: calibri; text-transform: uppercase; font-size: 10px; letter-spacing: 1px;">
+<option value="">TEAMS</option>
+<!--1--><option value="Atlanta Hawks">Atlanta Hawks</option>
+<!--2--><option value="Boston Celtics">Boston Celtics</option>
+<!--3--><option value="Brooklyn Nets">Brooklyn Nets</option>
+<!--4--><option value="Charlotte Hornets">Charlotte Hornets</option>
+<!--5--><option value="Chicago Bulls">Chicago Bulls</option>
+<!--6--><option value="Cleveland Cavaliers">Cleveland Cavaliers</option>
+<!--7--><option value="Dallas Mavericks">Dallas Mavericks</option>
+<!--8--><option value="Denver Nugget">Denver Nugget</option>
+<!--9--><option value="Detroit Pistons">Detroit Pistons</option>
+<!--10--><option value="Golden State Warriors">Golden State Warriors</option>
+<!--11--><option value="Houston Rockets">Houston Rockets</option>
+<!--12--><option value="Indiana Pacers">Indiana Pacers</option>
+<!--13--><option value="LA Clippers">LA Clippers</option>
+<!--14--><option value="Los Angeles Lakers">Los Angeles Lakers</option>
+<!--15--><option value="Memphis Grizzlies">Memphis Grizzlies</option>
+<!--16--><option value="Miami Heat">Miami Heat</option>
+<!--17--><option value="Milwaukee Bucks">Milwaukee Bucks</option>
+<!--18--><option value="Minnesota Timberwolves">Minnesota Timberwolves</option>
+<!--19--><option value="New Orleans Pelicans">New Orleans Pelicans</option>
+<!--20--><option value="New York Knicks">New York Knicks</option>
+<!--21--><option value="Oklahoma City Thunder">Oklahoma City Thunder</option>
+<!--22--><option value="Orlando Magic">Orlando Magic</option>
+<!--23--><option value="Philadelphia 76ers">Philadelphia 76ers</option>
+<!--24--><option value="Phoenix Suns">Phoenix Suns</option>
+<!--25--><option value="Portland Trail Blazers">Portland Trail Blazers</option>
+<!--26--><option value="Sacramento Kings">Sacramento Kings</option>
+<!--27--><option value="San Antonio Spurs">San Antonio Spurs</option>
+<!--28--><option value="Toronto Raptors">Toronto Raptors</option>
+<!--29--><option value="Utah Jazz">Utah Jazz</option>
+<!--30--><option value="Washington Wizards">Washington Wizards</option>
+
+</select>
+<br>
+<br>
+<select id = "basketball2" name="basketball2" style="width: 200px; border: 0px solid; padding: 5px; color: #000; background-color: #f1f1f1;  font-family: calibri; text-transform: uppercase; font-size: 10px; letter-spacing: 1px;">
+<option value="">TEAMS</option>
+<!--1--><option value="Atlanta Hawks">Atlanta Hawks</option>
+<!--2--><option value="Boston Celtics">Boston Celtics</option>
+<!--3--><option value="Brooklyn Nets">Brooklyn Nets</option>
+<!--4--><option value="Charlotte Hornets">Charlotte Hornets</option>
+<!--5--><option value="Chicago Bulls">Chicago Bulls</option>
+<!--6--><option value="Cleveland Cavaliers">Cleveland Cavaliers</option>
+<!--7--><option value="Dallas Mavericks">Dallas Mavericks</option>
+<!--8--><option value="Denver Nugget">Denver Nugget</option>
+<!--9--><option value="Detroit Pistons">Detroit Pistons</option>
+<!--10--><option value="Golden State Warriors">Golden State Warriors</option>
+<!--11--><option value="Houston Rockets">Houston Rockets</option>
+<!--12--><option value="Indiana Pacers">Indiana Pacers</option>
+<!--13--><option value="LA Clippers">LA Clippers</option>
+<!--14--><option value="Los Angeles Lakers">Los Angeles Lakers</option>
+<!--15--><option value="Memphis Grizzlies">Memphis Grizzlies</option>
+<!--16--><option value="Miami Heat">Miami Heat</option>
+<!--17--><option value="Milwaukee Bucks">Milwaukee Bucks</option>
+<!--18--><option value="Minnesota Timberwolves">Minnesota Timberwolves</option>
+<!--19--><option value="New Orleans Pelicans">New Orleans Pelicans</option>
+<!--20--><option value="New York Knicks">New York Knicks</option>
+<!--21--><option value="Oklahoma City Thunder">Oklahoma City Thunder</option>
+<!--22--><option value="Orlando Magic">Orlando Magic</option>
+<!--23--><option value="Philadelphia 76ers">Philadelphia 76ers</option>
+<!--24--><option value="Phoenix Suns">Phoenix Suns</option>
+<!--25--><option value="Portland Trail Blazers">Portland Trail Blazers</option>
+<!--26--><option value="Sacramento Kings">Sacramento Kings</option>
+<!--27--><option value="San Antonio Spurs">San Antonio Spurs</option>
+<!--28--><option value="Toronto Raptors">Toronto Raptors</option>
+<!--29--><option value="Utah Jazz">Utah Jazz</option>
+<!--30--><option value="Washington Wizards">Washington Wizards</option>
+
+</select>
+<br>
+<br>
+<select id = "basketball3" name="basketball3" style="width: 200px; border: 0px solid; padding: 5px; color: #000; background-color: #f1f1f1;  font-family: calibri; text-transform: uppercase; font-size: 10px; letter-spacing: 1px;">
+<option value="">TEAMS</option>
+<!--1--><option value="Atlanta Hawks">Atlanta Hawks</option>
+<!--2--><option value="Boston Celtics">Boston Celtics</option>
+<!--3--><option value="Brooklyn Nets">Brooklyn Nets</option>
+<!--4--><option value="Charlotte Hornets">Charlotte Hornets</option>
+<!--5--><option value="Chicago Bulls">Chicago Bulls</option>
+<!--6--><option value="Cleveland Cavaliers">Cleveland Cavaliers</option>
+<!--7--><option value="Dallas Mavericks">Dallas Mavericks</option>
+<!--8--><option value="Denver Nugget">Denver Nugget</option>
+<!--9--><option value="Detroit Pistons">Detroit Pistons</option>
+<!--10--><option value="Golden State Warriors">Golden State Warriors</option>
+<!--11--><option value="Houston Rockets">Houston Rockets</option>
+<!--12--><option value="Indiana Pacers">Indiana Pacers</option>
+<!--13--><option value="LA Clippers">LA Clippers</option>
+<!--14--><option value="Los Angeles Lakers">Los Angeles Lakers</option>
+<!--15--><option value="Memphis Grizzlies">Memphis Grizzlies</option>
+<!--16--><option value="Miami Heat">Miami Heat</option>
+<!--17--><option value="Milwaukee Bucks">Milwaukee Bucks</option>
+<!--18--><option value="Minnesota Timberwolves">Minnesota Timberwolves</option>
+<!--19--><option value="New Orleans Pelicans">New Orleans Pelicans</option>
+<!--20--><option value="New York Knicks">New York Knicks</option>
+<!--21--><option value="Oklahoma City Thunder">Oklahoma City Thunder</option>
+<!--22--><option value="Orlando Magic">Orlando Magic</option>
+<!--23--><option value="Philadelphia 76ers">Philadelphia 76ers</option>
+<!--24--><option value="Phoenix Suns">Phoenix Suns</option>
+<!--25--><option value="Portland Trail Blazers">Portland Trail Blazers</option>
+<!--26--><option value="Sacramento Kings">Sacramento Kings</option>
+<!--27--><option value="San Antonio Spurs">San Antonio Spurs</option>
+<!--28--><option value="Toronto Raptors">Toronto Raptors</option>
+<!--29--><option value="Utah Jazz">Utah Jazz</option>
+<!--30--><option value="Washington Wizards">Washington Wizards</option>
+
+</select>
+<!--<input value="Select" type="bu/>-->
+<br>
+<br>
+<input value="Select" type="submit"/>
+</center>
+</div>
+<!--<input value="Select" type="submit"/>-->
+</center>
+</div>
+<!--<input value="SELECT" type ="button" onclick="sndReq()"/>-->
+<!--<button class ="btn btn-lg btn-primary btn-block" type="button" onclick="sndReq()">Submit</button>-->
+</center>
+</center>
+<br>
+<br>
+<center>
+</center>
+<br>
+</body>
+</body>
+</html>
