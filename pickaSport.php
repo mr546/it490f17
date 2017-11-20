1171dfd340d0605b7f6e8e156daca354b6bdd020
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-11-20 16:00:45 -0500

    pickaSport.php

---
+++ pickaSport.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kehoe's Bros Betting</title> 
+<style>
+body {margin: 0;}
+#css1{
+  background-color: black;
+  background-position: right bottom;
+  background-repeat: repeat;
+  padding: .5px;
+}
+.topnav{
+    overflow: hidden;
+    background-color:black;
+    }
+.topnav a{
+    float: left;
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 5px 10px;
+    text-decoration: none;
+    font-size: 10px;
+    }
+.topnav a:hover{
+    background-color: lightblue;
+    color: white;
+}
+.topnav a.active{
+    background-color: #ee2211;
+    color: red;
+}   
+.box1{
+    position: relative;
+    margin: auto;
+    float: center;
+    width: 400px;
+    font-family:"georgia";
+    height: 100px;
+    background-color: pink;
+    text-align: left;
+    padding-left: .5cm;
+    padding-top: .05cm;
+    padding-bottom: .8cm;
+}
+    
+</style>
+
+</head>
+
+<body>
+<div id = "css1">
+<h3><font face="georgia" size="5" color="white">Kehoe's Bros Betting</font></h3>
+    <div class="topnav" id="firstTopNav">
+        <a href="homepage.php">HOME</a>
+        <a href="about.php">ABOUT</a>
+    </div>
+</div>
+</body>
+</head>
+
+<body>
+
+<p><center><b><font face="georgia" size="20" color="black">CHOOSE A SPORT:</font></b></center></p>
+
+<center>
+
+<a href="NBAhomepage.php">
+    <img src ="http://www.gameongrafix.com/sc_images/products/NBA%20Jam%20sideart-sca1-1000.jpg" style="border:0px;margin:0px;height:350px;width:200;">
+</a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="homepage.php">
+    <img src="https://images.complex.com/complex/image/upload/c_limit,w_680/fl_lossy,pg_1,q_auto/dtcgpflfr7yxaucc7sfz.jpg" style="border:0px;margin:0px;clear:both;height:350px; width:200;">
+</a>
+</center>
+</body>
+
+
