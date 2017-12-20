95f1a011a720339da6cb525bb9ddb4b8f4bf15dc
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-12-20 17:11:14 -0500

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -15,8 +15,8 @@ body {margin: 0;}
     background-color:black;
     }
 .topnav a{
-    float: left;
-    display: block;
+    float: center;
+    display: inline;
     color: white;
     text-align: center;
     padding: 5px 10px;
@@ -27,42 +27,24 @@ body {margin: 0;}
     background-color: lightblue;
     color: black;
 }
-.topnav a.active{
-    background-color: #ee2211;
-    color: red;
-}   
-.box1{
-    position: relative;
-    margin: auto;
-    float: center;
-    width: 400px;
-    font-family:"georgia";
-    height: 150px;
-    background-color: black;
-    color:white;
-    text-align: left;
-    padding-left: .5cm;
-    padding-top: .05cm;
-    padding-bottom: .8cm;
-}
-    
 </style>
-
 </head>
 
 <body>
 <div id = "css1">
+<center>
 <h3><font face="georgia" size="5" color="white" text-align="left" float="left">Kehoe's Bros Betting</font></h3> 
-<!--<h3> <font face="georgia" size="8" color="white" text-align="center" float="center">WELCOME!</font></h3></center>--> <!--MUST BE THE SAME LINE AS KEHOES BROS!!!!!-->
     <div class="topnav" id="firstTopNav">
-        <a href="homepage.php">HOME</a>
+        <a href="pickaSport.php">HOME</a>
         <a href="deposit.php">DEPOSIT</a>
         <a href="about.php">ABOUT</a>
-        <a href="compareResults.php">RESULTS</a> <!--I NEED ACCESS TO THE DB-->
+<!--        <a href="compareResults.php">RESULTS</a> <!--I NEED ACCESS TO THE DB-->-->
     </div>
+</center>
 </div>
 </body>
-<style>
+
+<!--<style>
     { margin: 0; padding: 0; }
     html {
         background: url('https://i.pinimg.com/originals/2c/ba/0c/2cba0cb95b6bfdad2a7699490309d8ff.jpg') no-repeat center center fixed;
@@ -71,38 +53,47 @@ body {margin: 0;}
         -o-background-size: cover;
         background-size: cover;
     }
+</style>-->
+
+<style>
+    { margin: 0; padding: 0; }
+    html {
+        background: white;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+    .divBack{
+    position: relative;
+    margin: 3%;
+    float: center;
+    width: 45%;
+    height: 50%;
+    padding-bottom: .5cm;
+    border: 2px solid black;
+/*    background-color: black|transparent;
+    background: rgba (255, 255, 255, .5);*/
+    }
 </style>
-
-</style>
-
-<?php
-session_start();
-  $username= $_SESSION["screenname"];
-  $amount= $_SESSION["amount"];
-?>
-
-<!--<form action = "depositingmoney.php" method ="get">-->
-
-<!--<?php   
-    print "Welcome $username <br>"; //need session
-    print "Balance: $ $amount <br><br>"; //need session
-?>-->
 
 <center>
 <br>
 <br>
+
 <body>
-<div class="box1">
-<form action = "depositingmoney.php" method ="get" style="width: 400px; padding-bottom: .8cm; padding-top: .05cm; background-color: #000000;">
 
-<h2><font face="georgia" size="3" color="white"><center>Place deposit money below </center></font></h2>
+<div class ="divBack"><!--
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="width: 400px; padding-bottom: .8cm; padding-top: .05cm; background-color: #000000; background-color: #000000; background-color: black| transparent; background: rgba(225, 225, 225, .59);">-->
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<h2><font face="georgia" size="3" color="black"><center>Place deposit money below </center></font></h2>
 
-    <label for="amount">Deposit Amount: </label>
-    <input type = "text" name="amount"  placeholder="Enter Amount"/>
-    <br>
-    <br>
-    <center><input type = "submit"></center>
-</div>
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="XZPAASUHB9RPJ">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+</div>
+
 </body>
 </html>
