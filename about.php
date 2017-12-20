c624cc81c996d20be8a68a1b69c1d7f2c60b1c5e
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-12-20 17:10:14 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kehoe's Bros Betting</title> 
+<style>
+body {margin: 0;}
+#css1{
+  background-color: black;
+  background-position: right bottom;
+  background-repeat: repeat;
+  padding: .5px;
+}
+.topnav{
+    overflow: hidden;
+    background-color:black;
+    }
+.topnav a{
+    float: center;
+    display: inline;
+    color: white;
+    padding: 5px 10px;
+    text-decoration: none;
+    font-size: 10px;
+    }
+.topnav a:hover{
+    background-color: lightblue;
+    color: white;
+}    
+</style>
+</head>
+
+<body>
+<center>
+<div id = "css1">
+<h3><font face="georgia" size="5" color="white">Kehoe's Bros Betting</font></h3>
+    <div class="topnav" id="firstTopNav">
+        <a href="pickaSport.php">HOME</a>
+        <a href="deposit.php">DEPOSIT</a>
+        <a href="about.php">ABOUT</a>
+        <!--<a href="compareResults.php">RESULTS</a>-->
+    </div>
+</div>
+</center>
+</body>
+
+<style>
+    { margin: 0; padding: 0; }
+    html {
+        background: url("https://usatftw.files.wordpress.com/2017/05/nba-logo.jpg?w=1000")no-repeat center center fixed;
+        /*background: white;*/
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+    .divBack{
+    position: relative;
+    margin: 3%;
+    float: center;
+    width: 60%;
+    height: 70%;
+    padding: 10px;
+    border: 2px solid black;
+    background-color: white; /*|transparent;
+    background: rgba (255, 255, 255, .5);*/
+    }
+</style>
+
+<body>
+<center>
+<div class ="divBack">
+    <h4><center>About Kehoe's Bros Betting</center></h4>
+    <p align="left">The website will track NFL quarterback, running backs, and wide receiver stats. Specifically their pass yards, rushing yards, and receiving yards. Users will be able to log into the website and have a balance that they can add money to bet with. The user will select a quarterback, running back, and wide receiever whom they believe will have the most passing yards, running yards, and receiving yards for the week. Once they select a player, they will place their bet. At the end of the week, the user will click the Weekly Results button on their homepage which will show them if their picks have won them money or not.
+    </p>
+    <p align="left"> As for NBA, our website traacks the games that are going on and uses an API to get hte games played every night. Our users are able to pick 3 teams they believe will win and will receive the same odds as the NFL bets if their choices won. 
+    </p>
+    <p align="left">
+        ~Gillky, Miguelley, Franky, Captain~
+    </p>
+</div>
+</center>
+
+</body>
+</html>
