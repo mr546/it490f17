584ab8bb5f3aed6628ce7695700412180648caf5
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-12-20 23:10:46 -0500

    Update deposit.php

--- deposit.php
+++ deposit.php
@@ -5,11 +5,11 @@
 <style>
 body {margin: 0;}
 #css1{
-  background-color: black;
-  background-position: right bottom;
-  background-repeat: repeat;
-  padding: .5px;
-}
+    background-color: black;
+    background-position: right bottom;
+    background-repeat: repeat;
+    padding: .5px;
+    }
 .topnav{
     overflow: hidden;
     background-color:black;
@@ -18,7 +18,7 @@ body {margin: 0;}
     float: center;
     display: inline;
     color: white;
-    text-align: center;
+/*    text-align: center;*/
     padding: 5px 10px;
     text-decoration: none;
     font-size: 10px;
@@ -27,73 +27,82 @@ body {margin: 0;}
     background-color: lightblue;
     color: black;
 }
+.topnav a.active{
+    background-color: #ee2211;
+    color: red;
+}   
+.box1{
+    position: relative;
+    margin: auto;
+    float: center;
+    width: 400px;
+    border: 2px solid;
+    font-family:"georgia";
+    height: 120px;
+    background-color: black;
+    color: black;
+    text-align: left;
+    padding-left: .5cm;
+    padding-top: .05cm;
+    padding-bottom: .8cm;
+    background-color: black| transparent;
+    background: rgba(225, 225, 225, .59);
+}    
 </style>
 </head>
+
+<style>
+    { margin: 0; padding: 0; }
+    html {
+        background: url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/VYJZA0Avg/rack-focus-of-stadium-lights-in-front-of-a-dark-blue-sky-light-stadium-at-night-baseball-and-hockey-there-is-a-rain-or-snow-military-security-lighting-towers-patrol-in-prison_h9xhm5hjle_thumbnail-full01.png') no-repeat center center fixed;
+       /* background-color: white;*/
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
 
 <body>
 <div id = "css1">
 <center>
-<h3><font face="georgia" size="5" color="white" text-align="left" float="left">Kehoe's Bros Betting</font></h3> 
+<h3><font face="georgia" size="5" color="white">Kehoe's Bros Betting</font></h3>
     <div class="topnav" id="firstTopNav">
         <a href="pickaSport.php">HOME</a>
         <a href="deposit.php">DEPOSIT</a>
         <a href="about.php">ABOUT</a>
-<!--        <a href="compareResults.php">RESULTS</a> <!--I NEED ACCESS TO THE DB-->-->
     </div>
 </center>
 </div>
 </body>
 
-<!--<style>
-    { margin: 0; padding: 0; }
-    html {
-        background: url('https://i.pinimg.com/originals/2c/ba/0c/2cba0cb95b6bfdad2a7699490309d8ff.jpg') no-repeat center center fixed;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-    }
-</style>-->
 
-<style>
-    { margin: 0; padding: 0; }
-    html {
-        background: white;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-    }
-    .divBack{
-    position: relative;
-    margin: 3%;
-    float: center;
-    width: 45%;
-    height: 50%;
-    padding-bottom: .5cm;
-    border: 2px solid black;
-/*    background-color: black|transparent;
-    background: rgba (255, 255, 255, .5);*/
-    }
-</style>
+<?php
+// session_start();
+//   $username= $_SESSION["screenname"];
+//   $amount= $_SESSION["amount"];
+// ?>
+
+ <?php  
+//     print "Welcome $username <br>"; //need session
+//     print "Balance: $ $amount <br><br>"; //need session
+// ?>
 
 <center>
 <br>
 <br>
-
 <body>
 
-<div class ="divBack"><!--
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" style="width: 400px; padding-bottom: .8cm; padding-top: .05cm; background-color: #000000; background-color: #000000; background-color: black| transparent; background: rgba(225, 225, 225, .59);">-->
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<form action = "depositingmoney.php" method ="get" style="width: 410px; padding-bottom: .8cm; padding-top: .05cm; border: 2px solid;background-color: black| transparent; background: rgba(225, 225, 225, .65);">
+<!--style="width: 400px; padding-bottom: .8cm; padding-top: .05cm; background-color: #000000;"-->
 <h2><font face="georgia" size="3" color="black"><center>Place deposit money below </center></font></h2>
 
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="XZPAASUHB9RPJ">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
+    <label for="amount">Deposit Amount: </label>
+    <input type = "text" name="amount"  placeholder="Enter Amount"/>
+    <br>
+    <br>
+    <center><input type = "submit"></center>
 </div>
-
+</form>
 </body>
 </html>
