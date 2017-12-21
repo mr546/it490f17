be5a0cc61f41308b31c1f244af4f2924bf02a3da
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-12-21 11:47:07 -0500

    Create sendit.sh
    deployment

---
+++ sendit.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sshpass -p miguelle scp bet.php depositingmoney.php index.html signup.html database1.php deposit.php insertBets.php signup.php database.php homepage.php compareResults.php host.ini local.ini login.php NBAbet.php depositingmoney.php deposit.php NBAhomepage.php NBAinsertBets.php path.inc signin.css pickaSport.php about.php currentBalance.php database.php NBAcompareResults.php  192.168.1.120:/var/www/html/
