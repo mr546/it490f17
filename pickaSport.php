13f9264cb38f4884de05630dd262d527bc539d58
Miguelle Reynolds <31661453+mr546@users.noreply.github.com>
2017-12-20 23:09:23 -0500

    Update pickaSport.php

--- pickaSport.php
+++ pickaSport.php
@@ -15,8 +15,8 @@ body {margin: 0;}
     background-color:black;
     }
 .topnav a{
-    float: left;
-    display: block;
+    float: center;
+    display: inline;
     color: white;
     text-align: center;
     padding: 5px 10px;
@@ -44,36 +44,55 @@ body {margin: 0;}
     padding-top: .05cm;
     padding-bottom: .8cm;
 }
-    
 </style>
-
 </head>
 
-<body>
+<!--<body>
+// <?php 
+// 
+// include ("currentBalance.php"); 
+// 
+// echo $userBalanceResult;
+// 
+// ?>
+</body>-->
+
+
+<head>
 <div id = "css1">
+<center>
 <h3><font face="georgia" size="5" color="white">Kehoe's Bros Betting</font></h3>
     <div class="topnav" id="firstTopNav">
-        <a href="homepage.php">HOME</a>
+        <a href="pickaSport.php">HOME</a>
+        <a href="deposit.php">DEPOSIT</a>
         <a href="about.php">ABOUT</a>
     </div>
+</center>
 </div>
 </body>
 </head>
 
-<body>
+<style>
+    { margin: 0; padding: 0; }
+    html {
+        background-color: white;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
 
+
+<body>
 <p><center><b><font face="georgia" size="20" color="black">CHOOSE A SPORT:</font></b></center></p>
 
 <center>
-
-<a href="NBAhomepage.php">
+    <a href="NBAhomepage.php">
     <img src ="http://www.gameongrafix.com/sc_images/products/NBA%20Jam%20sideart-sca1-1000.jpg" style="border:0px;margin:0px;height:350px;width:200;">
-</a>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="homepage.php">
+    </a>	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="homepage.php">
     <img src="https://images.complex.com/complex/image/upload/c_limit,w_680/fl_lossy,pg_1,q_auto/dtcgpflfr7yxaucc7sfz.jpg" style="border:0px;margin:0px;clear:both;height:350px; width:200;">
-</a>
+    </a>
 </center>
 </body>
-
-
